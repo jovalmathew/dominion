16f19b3f97119a61c3bf2ada2a528c91972490b8
jovalmathew <jovalmathew@gmail.com>
2017-02-22 23:49:17 -0500

    Basic structure functional. Now I need to add all Action cards and their effects

---
+++ baseset.py
@@ -0,0 +1,7 @@
+base_set = {'Adventurer': (1,0,0,0,6,10), 'Bureaucrat': (1,0,0,0,4,10), 'Cellar': (1,0,0,0,2,10), 'Chancellor': (1,0,0,0,3,10), 'Chapel': (1,0,0,0,2,10),
+			'Council Room': (1,0,0,0,5,10), 'Feast': (1,0,0,0,4,10), 'Festival': (1,0,0,0,5,10), 'Laboratory': (1,0,0,0,5,10), 'Library': (1,0,0,0,5,10), 
+			'Market': (1,0,0,0,5,10), 'Militia': (1,0,0,0,4,10), 'Mine': (1,0,0,0,5,10), 'Moat': (1,0,0,0,2,10), 'Money Lender': (1,0,0,0,4,10), 
+			'Remodel': (1,0,0,0,4,10), 'Smithy': (1,0,0,0,4,10), 'Spy': (1,0,0,0,4,10), 'Thief': (1,0,0,0,4,10), 'Throne Room': (1,0,0,0,4,10), 
+			'Village': (1,0,0,0,3,10), 'Witch': (1,0,0,0,5,10), 'Woodcutter': (1,0,0,0,3,10), 'Workshop': (1,0,0,0,3,10), 'Gardens': (0,0,1,0,4,8)}
+
+# (Action, Treasure, Victory, Curse, Cost,  Supply)
--- dom_classes.py
+++ dom_classes.py
@@ -1,167 +1,29 @@
 #!/usr/bin/env python3
 
-""" This defines the classes used in games of Dominion"""
+# C:\Users\Owner\AppData\Local\Programs\Python\Python35-32\python.exe C:\Users\Owner\Documents\Github\dominion\dom_classes.py 
+""" This defines the classes used in games of Dominion
+	We will use a list for everything, except for cards that have effects. 
+	These cards will call a dictionary from the base set, in which the effects will be called."""
 
-# What to do next:  Write the Action and Buy Phases, How to Play a Card? Also write Initialize Function. How to populate Field with Supply Piles?
-# Start the main function. In it, we can initialize the game object as a global object. That way, any function can access it
-# Create an iterator for Deck,DiscardPile,Trash,and Hand?
+# What to do next: Create a visualization for Kingdom Cards and their costs. Implement Action Cards!!!!
+# Make sure that I know where we're drawing from and keep that consistent.
 # implement a time limit
 
 import random
 from collections import Counter # This is a dictionary that can be added with other dictionaries
-# import baseset
-class Card():
-	def __init__(self,Name,Cost, Action=0,Treasure=0,Victory=0,Curse=0):
-		""" Action, Treasure, and Victory are booleans that indicate the type of a card. Some cards can be multiple types, which is why they aren't exclusive.
-		Cost is an non-negative integer indicating the cost of the card."""
-		self.Action= Action
-		self.Treasure=Treasure
-		self.Victory=Victory
-		self.Curse=Curse
-		self.Cost=Cost 
-		self.name = Name
-	def __repr__(self):
-		return self.name
-	# def __cmp__(self,other):
-	# 	return cmp(self.name, other.name)
+from baseset import *
 
-	def playCard(self):
-		# Removes Card from Hand such that its effect takes place
-		pass
+# The value of these dictionaries is a TUPLE (act_tup, treas_tup, vict_tup, curse_tup, cost_tup), which represents 
+# (Action, Treasure, Victory, Curse, Cost, Number in Supply Piles).  The treasure and victory components can be greater than one,
+# which represents their value. the Cost components represents how much they cost to buy. 
+act_tup = 0
+treas_tup = 1
+vict_tup = 2
+curse_tup = 3
+cost_tup = 4
+supply_tup = 5
+common_cards = {'Copper':(0,1,0,0,0,60),'Silver':(0,2,0,0,3,40),'Gold':(0,3,0,0,6,30),'Estate':(0,0,1,0,2,8),'Duchy':(0,0,3,0,5,8),'Province':(0,0,6,0,8,8),'Curse':(0,0,0,1,0,10)}
 
-
-class Kingdom():
-	# A Kingdom card is initialized to have the same methods as a Card(), but will have the additional methods associated with playing Action Cards
-	def __init__(self,Cost, Action=0,Treasure=0,Victory=0,Curse=0):
-		self.card=Card(self.Cost, self.Action,self.Treasure,self.Victory,self.Curse)
-		self.effectdict = Counter({"Actions":0,"Buys":0,"Money":0}) # This will be altered by a specific card's effect
-
-	def modActions(dictionary, numActions):
-		# This function will simply return the integer passed to it in a dictionary. This represents a modification on the number of Actions in a Player's turn
-		dictionary["Actions"]+=numActions
-		return dictionary
-
-	def modDraw(numDraws,deck):
-		# This function will allow the user to draw an integer numDraws number of cards from the Deck
-		cardsdrawn= draw(numDraws,deck)
-		return cardsdrawn # We can then append this to the hand
-
-	def modBuys(dictionary, numBuys):
-		dictionary["Buys"]+=numBuys
-		return dictionary
-
-	def modMoney(dictionary, numMoneys):
-		dictionary["Money"]+=numMoneys
-		return dictionary
-
-	def modTrash(num,options="True"):
-		# Places cards in the Trash. Sometimes this allows us to trash multiple cards. Sometimes we have the option to choose which card(s) to trash. 
-		pass
-
-	def GainfromSupply():
-		# Take Cards from the Supply Pile, send it to Discard Pile 
-		pass
-
-	def modDiscard():
-		pass
-
-	def Reveal():
-		pass	
-
-	def SetAside():
-		pass
-
-	def PlayAnother():
-		pass
-
-# This cards appear in every set, Copper, Silver, Gold; Estate, Duchy, Province; Curse
-class Copper():
-	def __init__(self):
-		self.card=Card(Name = "Copper",Treasure=1,Cost=0)
-		self.Value = 1
-	def __repr__(self):
-		return str(self.card)
-
-class Silver():
-	def __init__(self):
-		self.card=Card(Name = "Silver",Treasure=1,Cost=3)
-		self.Value = 2	
-	def __repr__(self):
-		return str(self.card)
-
-
-class Gold():
-	def __init__(self):
-		self.card=Card(Name = "Gold",Treasure=1,Cost=6)
-		self.Value = 3	
-	def __repr__(self):
-		return str(self.card)
-
-
-
-
-class Estate():
-	def __init__(self):
-		self.card=Card(Name = "Estate",Victory=1,Cost=2)
-		self.Points = 1
-	def __repr__(self):
-		return str(self.card)
-
-
-
-class Duchy():
-	def __init__(self):
-		self.card=Card(Name = "Duchy",Victory=1,Cost=5)
-		self.Points = 3
-	def __repr__(self):
-		return str(self.card)
-
-
-
-class Province():
-	def __init__(self):
-		self.card=Card(Name = "Province",Victory=1,Cost=8)
-		self.Points = 6
-	def __repr__(self):
-		return str(self.card)
-
-
-
-
-class Curse():
-	def __init__(self):
-		self.card=Card(Name = "Curse",Curse=1,Cost=0)
-		self.Points = -1
-	def __repr__(self):
-		return str(self.card)
-
-
-# Now I need a Deck, and in that, methods to draw cards, check the amount of cards left
-class Deck():
-	def __init__(self):
-		# Initialized with 7 Coppers and 3 Estates in a random order
-		# We play as if the last card in the list is on the top of the deck.
-		self.ActualCards=[Copper(),Copper(),Copper(),Copper(),Copper(),Copper(),Copper(),Estate(),Estate(),Estate()]
-		self.OutofPlay=[] #Cards that have been removed from the game (not trashed)
-		random.shuffle(self.ActualCards)
-
-	def __repr__(self):
-		shuffled = self.ActualCards[:]
-		random.shuffle(shuffled)
-		return str(shuffled[:])
-
-	def __iter__(self):
-		return iter(self.ActualCards)
-
-	def drawNewHand(self):
-		# Take an amount of cards from the deck, and put it in the player's hand
-		newHand = move_cards(5,self.ActualCards)
-		hand = Hand(newHand) # Could this cause errors with naming?
-		return hand
-
-class DiscardPile():
-	def __init__(self):
-		self.CardsDiscarded = []
 		
 # Next, we can make a list of Card instances that make up the hand. This will have methods associated with it as well
 class Hand():
@@ -179,32 +41,69 @@ class Player():
 	# Each Player is initialized with a standard Deck
 	def __init__(self,ID):
 		self.PlayerID = ID
-		self.Deck = Deck()
-		self.VictoryPoints = 0 
+		self.Deck = ['Copper','Copper','Copper','Copper','Copper',
+					'Copper','Copper','Estate','Estate','Estate'] 
+		random.shuffle(self.Deck)
+		self.Discard = []
+		self.InPlay = []		# When Cards are played out of the hand, they are placed here, then moved to Discard at the end of the turn
+		self.Hand = self.draw_hand()
+
+
 		# self.name=""
 	def __str__(self):
 		return("Player%s" %(self.PlayerID))
+
+	def draw_hand(self):
+		# Take an amount of cards from the deck, and put it in the player's hand
+		# Shuffle discard pile into deck if there are no cards left.
+		if len(self.Deck)<5:
+			# This is much too long and cumbersome
+			len_mod = 5 - len(self.Deck)%5
+			newHand_1 = move_cards(len(self.Deck),self.Deck)
+			# print("not enough cards!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+			self.Deck = list(self.Discard)
+			random.shuffle(self.Deck)
+			self.Discard = []
+			newHand_2 = move_cards(len_mod,self.Deck)
+			newHand = newHand_1+newHand_2
+		else:
+			newHand = move_cards(5,self.Deck)
+		return newHand
+
+
+	def put_in_play(self,card_index, source):
+		# card_name must be a string that exists as a key in the cards dictionary
+		# source must be a list
+
+		# move card to InPlay list
+		self.InPlay.append(source.pop(card_index))
 
 # Game Hierarchy
 # We can instantiate an instance of Game() to start playing. THis creates a deck for all players, randomizes the set of Action cards, designates 
 # a player to go first. 
 class Game():
+
+		 
+
 	def __init__(self,NumofPlayers):
 		# A game is initialized with an integer amount of players, with each player receiving a deck of 10 cards
 		# self.PlayerList= []
 		# self.PlayerName = ""
 		self.Players = [Player(i) for i in range(NumofPlayers)]
+		self.Trash = []
+		#### Figure out the Kingdom cards for use this game, and create the supply piles. Also add the kingdom cards to the common_cards dictionary
 
+		kingdom_cards = dict(random.sample(list(base_set.items()),10)) # This takes 10 random cards from the base_set, puts them in the new dictionary kingdom_cards
+		common_cards.update(kingdom_cards) # This adds the newly minted kingdom cards to the game.
 
-		# We can also come up with the randomized list of Actions used this game
+		common_list = list(common_cards.items())
+		supply_keys = [i[0] for i in common_list]
+		supply_vals = [i[1][supply_tup] for i in common_list]
 
-class Field():
-	# The field consists of everything common to both players, the 10 Kingdom Cards, the Trash, Treasures and Victory Cards
+		self.Supply = dict(zip(supply_keys, supply_vals)) # This creates the supply piles!
+		# Subtract out Coppers and add Victories if need be. 
+		self.Supply['Copper'] -= 7*NumofPlayers
 
-	def __init__():
-		# Initializing the field will create an empty trash, 10 Supply Piles, Victory Cards and Treasures
-		self.Trash=[] 
-		Supply = {}
 
 
 # General Functions for use by Deck, Hand, Trash, or any pile
@@ -214,11 +113,21 @@ def checkSize(listOfcards):
 	return len(listOfcards)
 
 def move_cards(amount,source):
+	# Pop, with no index, defaults to taking the last item out of the list
 	# Takes an amount of cards from the source, and moves it to the destination. This method is further extended in other classes
 	cardsdrawn = []
+	if amount == 0: # Return nothing if we are told to draw no cards
+		return cardsdrawn
+
 	for i in range(amount):
 		cardsdrawn.append(source.pop())
 	return cardsdrawn
+
+def discard_all(source, destination):
+	# Both destination and source must be lists
+	destination+=source
+	source = []
+
 def howmanyactions():
 	# A Player can Call this to check how many actions she has left
 	pass
@@ -240,51 +149,67 @@ def ActionPhase():
 	# This needs a way to play cards that the player chooses, and a way to implement their effects. The played cards also need to 
 	# be placed in a location where they can be moved to the discard pile at the end of the turn. 
 	# While action_count, allow Actions to be played 
+	curr_hand = curr_game.Players[currentPlayer].Hand # will change later
 	action_count = 1
 	effect = Counter({'Actions':1, 'Buys':1, 'Money':0})
 	while action_count:
-		cont =input("Would you like to play an Action? y/n")
+		cont =input("Would you like to play an Action? y/n: ")
 		#if cont, execute loop, else break. 
 		if cont == 'n': 
 			effect['Actions'] = 0
 			return effect
 
-		cardtoplay = input("What action will you play?")
+		cardtoplay = input("What action will you play? : ")
 
-		# Check if input card exists in hand
-		if any(x.card.name.lower() == cardtoplay.lower() for x in game1.Players[currentPlayer].hand.HandCards):
+		cardtoplay = cardtoplay.title()
+		# Check if input card exists in hand ([x.lower() for x in curr_hand])
+		if cardtoplay in curr_hand:
+			# Find the index
+			cardindex = curr_hand.index(cardtoplay)
 
-			# Really stupid way to find the relevant index in the players hand		
-			for x in range(0,len(game1.Players[currentPlayer].hand.HandCards)):
-				if game1.Players[currentPlayer].hand.HandCards[x].card.name.lower() == cardtoplay.lower():
-					cardindex = x
-					break
 			# Check if the card is an action card
-			if game1.Players[currentPlayer].hand.HandCards[cardindex].card.Action:
-				# Activate the effect of the card in question
-				effect = effect + game1.Players[currentPlayer].hand.HandCards[cardindex].effect()
-				# Take into account the effects (using a dictionary to alter Action, Buy, and Money counters)
-				action_count += effect.get('Actions',0) - 1
+			if common_cards[curr_hand[cardindex]][act_tup]:
+				# move card to in play
+				curr_game.Players[currentPlayer].put_in_play(cardindex,curr_hand)
+				# Call the effect of the card
+				## This needs to be done next!!!!!!!!!!!!!!!
 				return effect
 			else:
 				print("This card is not an Action card")
 		else:	
 			print("Card not in hand")
 			continue
+
 	return effect
 def BuyPhase(effects_dictionary):
 	# While CurrentPlayerBuys, allow Buys to be made
-	buy_count = effects_dictionary.get("Buys", 1)
-	print(effects_dictionary)
+	curr_hand = curr_game.Players[currentPlayer].Hand # will change later
+	buy_count = effects_dictionary.get("Buys", 1)	
 	# Count the number of treasure in our hand. Add that up with the money from the dictionary. Allow gaining a card from the supply piles, 
 	# subtract that cost from the available money. 
+	total_money = effects_dictionary.get("Money",0)
+	# print(curr_game.Supply)
+	for i in curr_hand:
+		total_money += common_cards[i][treas_tup]
 	while buy_count:
-		cont =input("Would you like to buy something? y/n")
+		cont =input("Would you like to buy something? y/n : ")
 		if cont == 'n': 
-			effect['Buys'] = 0
+			buy_count = 0
 			return
-		break
-	pass
+		else:
+			cardtobuy = input("What would you like to buy? :")
+			cardtobuy = cardtobuy.title()
+			# check if card exists in supply pile, and if we have enough money. if so, Add it to discard, subtract cost from total_money
+			if common_cards[cardtobuy][cost_tup]<=total_money and curr_game.Supply[cardtobuy]>0:
+				curr_game.Players[currentPlayer].Discard.append(cardtobuy)	# Add Card to Discard Pile (make a gain function) 
+				curr_game.Supply[cardtobuy] -= 1 							# Decrement Supply Pile
+				total_money -= common_cards[cardtobuy][cost_tup] 			# Decrease money
+				buy_count -=1  												# Decrease buys
+				
+			else: 
+				print('You cannot buy this card')
+
+	return 
 def ResidualEffects():
 	# This function will execute events that take place at the start of a players turn
 	pass
@@ -293,25 +218,32 @@ def ResidualEffects():
 def InitializeGame(numplayers):
 	# This function will instantiate a Game() object, and Set up the basics of the game. When finished, it will
 	# call CurrentplayerTurn
-	global game1, currentPlayer, totalplayers
-	totalplayers=numplayers
-	game1 = Game(totalplayers)
-	currentPlayer = 0
-	#Create Supply Piles
-	# Here, we have a list possible_cards, which consists of all sets in this game. From this list, we choose 10 cards. 
-	# new_list = random.sample(possible_cards, 10)
-	# {el:0 for el in new_list}
+	global curr_game, currentPlayer, totalplayers
 
-	for i in range(0,totalplayers):
-		game1.Players[i].hand= game1.Players[i].Deck.drawNewHand()
+	totalplayers=numplayers
+
+	curr_game = Game(numplayers)
+	currentPlayer = 0
+
+	# print(curr_game.Players[0].Deck)
+	# for i in range(0,numplayers):
+	# 	print(i)
+	# 	curr_game.Players[i].Hand= curr_game.Players[i].draw_hand() # This calls the drawing far too much!
 
 	CurrentplayerTurn(currentPlayer)
-	pass
 
 def CurrentplayerTurn(currentPlayer):
 	# This will check the global variable for the current player, and go through the phases of the turn
 	# any initial draws, then Action Phase, then Buy phase. After all of this, we can call EndofTurn(), 
 
+	curr_hand = curr_game.Players[currentPlayer].Hand
+	# print(currentPlayer)
+	# print('This is the deck')
+	# print(curr_game.Players[currentPlayer].Deck)
+	# print("This is the hand")
+	# print(curr_hand)
+	# print("This is the Discard Pile")
+	# print(curr_game.Players[currentPlayer].Discard)
 	effect = ActionPhase()
 	BuyPhase(effect)
 	EndofTurn()
@@ -322,28 +254,39 @@ def EndofTurn():
 	# change currentPlayer variable to the next player in the list (change a global variable?)
 	# Check to see if end of game conditions are met. If so, call EndofGame, else call CurrentplayerTurn
 
-	if True:
+	if list(curr_game.Supply.values()).count(0)>2 or ~curr_game.Supply['Province']==0:
 		EndofGame()
 	else: 
-
+		global currentPlayer
 		# Move everything to discard pile, draw new hand 
-		currentPlayer = (currentPlayer+1)%totalplayers
+		# print('This is the Discard Pile')
+		# print(curr_game.Players[currentPlayer].Discard)
+		discard_all(curr_game.Players[currentPlayer].Hand, curr_game.Players[currentPlayer].Discard)
+		discard_all(curr_game.Players[currentPlayer].InPlay, curr_game.Players[currentPlayer].Discard)
+
+		# print('This is the new Discard Pile at the end of the turn')
+		# print(curr_game.Players[currentPlayer].Discard)
+
+		# print('This is the current deck')
+		# print(curr_game.Players[currentPlayer].Deck)
+		curr_game.Players[currentPlayer].Hand= curr_game.Players[currentPlayer].draw_hand()
+		# print('This is the Deck after drawing')
+		# print(curr_game.Players[currentPlayer].Deck)
+		# Change Player
+		currentPlayer= (currentPlayer+1)%totalplayers
 		CurrentplayerTurn(currentPlayer)
 	pass
 
 def EndofGame():
 	# If end conditions are met, this function will add up the Victory points of all the players, and declare 
 	# the winner! After which it will ask the users to play again. If so, restart game, If not, exit
+
 	pass
 
 
 # Debugging
 if __name__ == '__main__':
 
-	# Tests
-	# print('Cards in Player%s\'s Deck: %s' %(game1.Players[0].PlayerID+1, game1.Players[0].Deck.ActualCards)) # Incongruity in printing the deck
-	# game1.Players[0].hand1= game1.Players[0].Deck.drawNewHand()
-	# cardtoplay = "Copper"
-	# print(game1.Players[0].hand1.HandCards)
-	# a = game1.Players[0].hand1.index(Estate)
-	InitializeGame(2)
+	numplayers = 2
+
+	InitializeGame(numplayers)
